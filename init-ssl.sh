7b43e40e9cae1f8ab788cb424d081b7a28d96d26
neonfoxik <neonfoxik1@gmail.com>
2025-12-30 17:29:22 +0300

    67

--- init-ssl.sh
+++ init-ssl.sh
@@ -2,7 +2,7 @@
 
 # Script to initialize SSL certificate for actiontest.ru
 
-echo "Starting SSL certificate initialization for actiontest.ru..."
+echo "Starting SSL certificate initialization for ter.actiontest.ru..."
 
 # Create certbot www directory
 mkdir -p certbot/www
@@ -21,15 +21,14 @@ docker compose run --rm certbot certonly \
   --email admin@actiontest.ru \
   --agree-tos \
   --no-eff-email \
-  -d actiontest.ru \
-  -d www.actiontest.ru
+  -d ter.actiontest.ru 
 
 # Check if certificate was obtained successfully
 if [ $? -eq 0 ]; then
     echo "SSL certificate obtained successfully!"
     echo "Restarting nginx with SSL configuration..."
     docker compose restart nginx
-    echo "SSL setup complete! Your site should now be available at https://actiontest.ru"
+    echo "SSL setup complete! Your site should now be available at https://ter.actiontest.ru"
 else
     echo "Failed to obtain SSL certificate. Please check the errors above."
     exit 1
